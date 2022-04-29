dda392b6347fa8c63e99f7c0da86006ffdbac340
Abraham-Varguez <102987905+Abraham-Varguez@users.noreply.github.com>
2022-04-29 14:30:53 -0400

    Week 5

--- code.js
+++ code.js
@@ -1,17 +1,48 @@
-let name = ['Mario', 'Luigi', 'Peach', 'Daisy', 'Waluigi']
-
-let wins = [10, 8, 6, 4, 2]
+//Friday ES6 in class coding assignment.  Please copy and paste your code into the text-box.
 
 
-let champion = [name[0] > name[1], name[1] > name[2],
-name[2] > name[3], name[3] > name[4], name[4] > name[5]]
-
-function newCharacterUnlocked() {
-    return name.push('Bowser')
+//1 Convert the following function into an arrow function named "arrowMyFunction".
+// const myFunction = function (num1, num2) {
+//     return num1 + num2;
+// };
+//Arrow Function 
+const arrowMyFunction = (num1, num2) => {
+    return num1 + num2
 }
 
-if (champion[0]) {
-    newCharacterUnlocked
-    console.log('Bowser has joined the Roster!!')
+console.log(arrowMyFunction(9, 10))
+//2 Convert the following function into a one-lined arrow function without a return statement (the return is implicit) . Name the function arrowAdd100
+// const add100 = function (money) {
+//     return money + 100;
+// };
+
+const add100 = money => money + 100;
+
+console.log(add100(3000))
+
+//3 Create an arrow function that takes TWO arguments, 
+// a string and a single letter. 
+//  The function will count the number of times the 
+// specified letter occurs in the string.
+
+
+let countLetters = (phrase, letter) => {
+    let count = 0;
+    for (let i = 0; i <= phrase.length; i++) {
+        if (phrase.charAt(i) == letter) {
+            count += 1
+        }
+
+    }
+    return count;
 }
+
+//phrase is the sentence
+// letter is how many letters of a specific one is in the sentence
+
+console.log(countLetters('Jodi, Jordon and Justin are the Goats', 'J'))
+
+
+
+
 
